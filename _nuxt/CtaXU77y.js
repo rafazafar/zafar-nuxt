f5284b300c6093d0a4a7319392a3cab475707162
rafazafar <rafazafar@gmail.com>
2025-05-02 16:17:59 +0000

    Deploying to gh-pages from @ rafazafar/zafar-nuxt@bc164ef9673e84bd333a3f822e8014031027cac4 🚀

--- _nuxt/CtaXU77y.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./BUefyHhS.js";import a from"./2cjwYhXc.js";import{y as e,I as n,h as s,w as c,E as i}from"./sx5zV9Kt.js";import"./BcoM21Dg.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
