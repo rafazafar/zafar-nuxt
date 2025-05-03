424910268d7aa80214cecbcc128a6e6cadbc5a99
rafazafar <rafazafar@gmail.com>
2025-05-03 11:10:56 +0000

    Deploying to gh-pages from @ rafazafar/zafar-nuxt@d9fca09a7064a2f757912aa966a968adfcfbc817 🚀

--- _nuxt/BgMYD_eV.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./BFA68vc9.js";import t from"./B6X9u82A.js";import{y as e,I as a,h as p,w as c,E as n}from"./CMEhCBNw.js";import"./BcoM21Dg.js";const d={__name:"ProseTip",setup(i){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
