26b3fd6bbe1acb51bc6d614881581b944d29e528
Ahmad Zafar <me@zafar.dev>
2025-06-14 19:55:19 +0900

    update blog paths

--- content/blog/walk-up-simple.md
+++ content/blog/walk-up-simple.md
@@ -1,7 +1,7 @@
 ---
 title: Walk-up Simple
 description: A guide to the Tesla's Walk Up Simple method and the Law of Two Feet
-date: 2025-03-15
+date: 2025-04-20
 image: https://images.unsplash.com/photo-1663008519747-9da38bbfd132?q=80&w=600&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
 minRead: 3
 author:
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,54 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui-pro',
-    '@nuxt/content',
-    '@vueuse/nuxt',
-    'nuxt-og-image',
-    'motion-v/nuxt'
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/ui-pro",
+    "@nuxt/content",
+    "@vueuse/nuxt",
+    "nuxt-og-image",
+    "motion-v/nuxt",
   ],
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
 
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
 
   nitro: {
-    prerender: {
-      routes: [
-        '/'
-      ],
-      crawlLinks: true
+    routeRules: {
+      "/blog/**": { static: true },
     },
-    experimental:{
-      openAPI: true
+    prerender: {
+      routes: ["/", "/blog"],
+      crawlLinks: true,
+    },
+    experimental: {
+      openAPI: true,
     },
     openAPI: {
-      production: 'runtime',
+      production: "runtime",
       ui: {
         scalar: {
-          theme: 'cyan'
-        }
-      }
-    }
-    
+          theme: "cyan",
+        },
+      },
+    },
   },
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
-})
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
+  },
+});
